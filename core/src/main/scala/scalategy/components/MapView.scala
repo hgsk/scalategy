aedf8b1357c6fb0b5089f5d0af2734ad833bf61f
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2017-05-21 18:57:28 +0900

    ドラッグによるマップ移動対応

--- core/src/main/scala/scalategy/components/MapView.scala
+++ core/src/main/scala/scalategy/components/MapView.scala
@@ -1,28 +1,53 @@
 package scalategy.components
 
+import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.assets.AssetManager
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.scenes.scene2d.{Actor, Group}
-import com.badlogic.gdx.scenes.scene2d.ui.{Image, Table}
-import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.graphics.g2d.Batch
+import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.{Actor, Group, InputEvent, InputListener}
 
+import scalategy.AppSettings
 import scalategy.common.UseAssets
 
-case class MapView() extends Group with Component {
+case class MapView()(implicit appSettings: AppSettings) extends Group with Component {
   import MapView._
+  import appSettings.stageHeight
+  val xOffset = 0
+  val yOffset = 30
   override def initialize(assetManager: AssetManager): Actor = {
-    val table = new Table()
     val squareTexture = assetManager.get[Texture](ASSET_SQUARE)
-    for (x <- 0 to 9; y <- 0 to 9) {
+    val mapSize = 20
+    val mapWidth = 800
+    val mapHeight = 540
+    val tileSize = 40
+    val minX = ((tileSize + 1) * mapSize - mapWidth - xOffset) * -1
+    val minY = ((tileSize + 1) * mapSize - mapHeight - yOffset) * -1
+    setPosition(xOffset, yOffset)
+    for (x <- 0 until mapSize; y <- 0 until mapSize) {
       val square = new Image(squareTexture)
-      square.setPosition(x * 101, y * 101)
+      square.setBounds(x * (tileSize + 1), y * (tileSize + 1), tileSize, tileSize)
       addActor(square)
     }
-    table.setClip(true)
-    table.setBounds(0, 0, 800, 520)
-    table.add(this).expand().align(Align.bottomLeft)
-    table
+    addListener(new InputListener() {
+      private var pos: (Float, Float) = _
+      override def touchDown(event: InputEvent, x: Float, y: Float, pointer: Int, button: Int): Boolean = {
+        pos = (Gdx.input.getX - getX, stageHeight - Gdx.input.getY - getY)
+        true
+      }
+      override def touchDragged(event: InputEvent, x: Float, y: Float, pointer: Int): Unit = {
+        setPosition(
+          (Gdx.input.getX - pos._1).max(minX).min(xOffset),
+          (stageHeight - Gdx.input.getY - pos._2).max(minY).min(yOffset)
+        )
+      }
+    })
+    this
   }
+  override def draw(batch: Batch, parentAlpha: Float): Unit = {
+    super.draw(batch, parentAlpha)
+  }
+
 }
 object MapView extends UseAssets {
   val ASSET_SQUARE = "square.png"
--- core/src/main/scala/scalategy/scenes/GameScene.scala
+++ core/src/main/scala/scalategy/scenes/GameScene.scala
@@ -5,12 +5,12 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont
 import com.badlogic.gdx.graphics.{Color, Texture}
 import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle
-import com.badlogic.gdx.scenes.scene2d.ui.{Label, Table}
-import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.scenes.scene2d.ui.{Image, Label}
 
+import scalategy.AppSettings
 import scalategy.components.MapView
 
-class GameScene extends Scene {
+class GameScene()(implicit val appSettings: AppSettings) extends Scene {
   val ASSET_CIRCLE = "circle.png"
   val ASSET_SQUARE = "square.png"
   val ASSET_DIAMOND = "diamond.png"
@@ -26,16 +26,23 @@ class GameScene extends Scene {
   override def enter(assetManager: AssetManager): Group = {
     val main = new Group
     val bitmapFont = assetManager.get[BitmapFont](ASSET_FONT)
-    val table = new Table()
+    val squareTexture = assetManager.get[Texture](ASSET_SQUARE)
     val labelStyle = new LabelStyle(bitmapFont, new Color(0xffffffff))
+    val headerBg = new Image(squareTexture)
+    val footerBg = new Image(squareTexture)
+    val headerLabel = new Label("Header", labelStyle)
+    headerBg.setBounds(0, 570, 800, 30)
+    headerBg.setColor(0, 0, 0, 1)
+    headerLabel.setPosition(10, 570)
     main.addActor(MapView().initialize(assetManager))
-    table.add(new Label("Header", labelStyle)).align(Align.left)
-    table.row()
-    table.add(main).expand().align(Align.bottomLeft)
-    table.row()
-    table.add(new Label("Footer", labelStyle)).align(Align.left)
-    table.setFillParent(true)
-    table
+    main.addActor(headerBg)
+    main.addActor(headerLabel)
+
+    footerBg.setBounds(0, 0, 800, 30)
+    footerBg.setColor(0, 0, 0, 1)
+    main.addActor(footerBg)
+    main.addActor(new Label("Footer", labelStyle))
+    main
   }
   override def update(assetManager: AssetManager): Unit = ()
   override def exit(assetManager: AssetManager): Unit = ()
