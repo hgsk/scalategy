736e65b90a9d4df32ca98609bf5b0d4e00f034bc
Yuichiro Iwai <yuichiro.iwai@gmail.com>
2017-06-04 09:38:53 +0900

    akka-http-sessionを導入

--- server/src/main/scala/scalategy/Server.scala
+++ server/src/main/scala/scalategy/Server.scala
@@ -1,9 +1,12 @@
 package scalategy
 
-import akka.actor.{ActorRef, ActorSystem}
+import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
+import com.softwaremill.session.SessionDirectives._
+import com.softwaremill.session.SessionOptions._
+import com.softwaremill.session.{SessionConfig, SessionManager}
 import upickle.default._
 
 import scala.util.Try
@@ -16,27 +19,51 @@ object Server extends App {
   implicit val materializer = ActorMaterializer()
   import system.dispatcher
 
+  val sessionConfig = SessionConfig.fromConfig()
+  implicit val sessionManager = new SessionManager[String](sessionConfig)
+
   val route =
-    (post & path("api" / Segments)) { segments =>
-      entity(as[String]) { s =>
-        complete {
-          AutowireServer.route[Api](new ApiImpl(ServerContext)) {
-            autowire.Core.Request(
-              segments,
-              upickle.default.read[Map[String, String]](s)
-            )
+    pathSingleSlash {
+      reject()
+    } ~
+      (post & path("session")) {
+        val playerSession = ServerContext.registerSession
+        setSession(oneOff, usingHeaders, playerSession.path.name) {
+          complete("")
+        }
+      } ~
+      (post & path("api" / Segments)) { segments =>
+        requiredSession(oneOff, usingHeaders) { session =>
+          entity(as[String]) { s =>
+            complete {
+              AutowireServer.route[Api](new ApiImpl(ServerContext)) {
+                autowire.Core.Request(
+                  segments,
+                  upickle.default.read[Map[String, String]](s)
+                )
+              }
+            }
           }
         }
       }
-    }
 
   Http().bindAndHandle(route, "0.0.0.0", 9000)
+}
+
+class PlayerSession extends Actor {
+  override def receive: Receive = {
+    case _ =>
+  }
+}
+object PlayerSession {
+  def props() = Props(classOf[PlayerSession])
 }
 
 trait ServerContextLike {
   protected var gameInfos: Seq[GameInfo] = Seq.empty
   protected var players: Map[String, Player] = Map.empty
   protected var sessions: Map[String, ActorRef] = Map.empty
+  def registerSession()(implicit system: ActorSystem): ActorRef = system.actorOf(PlayerSession.props())
   def registerPlayer(name: String): Try[Player] = Try(Player())
   def findSession(sessionKey: String): Option[ActorRef] = sessions.get(sessionKey)
   def findGameBySessionKey(sessionKey: String): Option[GameInfo] = gameInfos.find(_.sessionKey == sessionKey)
